595e1fc56b8fe038cd35217018bbfe193817579f
kadin van valin <kadin@kadin.codes>
2023-01-24 13:13:52 -0600

    [main] add zt

--- .config/fish/functions/zt.fish
+++ .config/fish/functions/zt.fish
@@ -1,5 +1,7 @@
 function zt
-    if test -f "$argv[1]"
+    if test -z "$argv[1]"
+        ls .
+    else if test -f "$argv[1]"
         bat "$argv[1]"
     else if  test -d "$argv[1]"
         ls "$argv[1]"
