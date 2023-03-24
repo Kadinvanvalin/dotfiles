b3f2340cece6152c476cfbd8f06bf699764201b3
kadin van valin <kadin@kadin.codes>
2023-03-24 15:28:36 -0500

    [main]

--- .config/fish/functions/kk.fish
+++ .config/fish/functions/kk.fish
@@ -1,10 +1,10 @@
 function ks
     if test -z "$argv[1]"
-        ls .
+        la .
     else if test -f "$argv[1]"
         bat "$argv[1]"
     else if  test -d "$argv[1]"
-        ls "$argv[1]"
+        la "$argv[1]"
     else
         echo "I don't know what that is"
     end
